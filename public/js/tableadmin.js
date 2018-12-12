f29aae667021071a09b76e98e8188308eda2f408
anne portier <39262353+aportier2018@users.noreply.github.com>
2018-12-12 16:20:41 +0100

    diver

--- public/js/tableadmin.js
+++ public/js/tableadmin.js
@@ -20,5 +20,29 @@ $(document).ready(function ()
                 "previous": "précédente"
                 }
             }
+        });
+        
+    $('#tablemotcle').DataTable(
+        {
+            "columnDefs": [
+                { "orderable": false, "targets": 2 },
+                { "orderable": false, "targets": 3 }
+            ],
+            "language":
+            {
+                "lengthMenu": "affichage de _MENU_ lignes par page",
+                "search": "Recherche",
+                "zeroRecords": "aucun résultat - Désolé",
+                "info": "nombre de page _PAGE_ of _PAGES_",
+                "infoEmpty": "Enregistrement invalide",
+                "infoFiltered": "(filtered from _MAX_ total records)",
+                "paginate":
+                {
+                    "next": "suivante",
+                    "previous": "précédente"
+                }
+            }
         })
+    
+    
 });
